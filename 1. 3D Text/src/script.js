b648ac239dba581fbce8f1bc4a99c71592189b07
Santiago López Viteri <77015177+Axl11475581@users.noreply.github.com>
2023-04-05 21:34:39 -0500

    test

--- 1. 3D Text/src/script.js
+++ 1. 3D Text/src/script.js
@@ -36,7 +36,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Bruno Simon',
+            'Test',
             {
                 font: font,
                 size: 0.5,
